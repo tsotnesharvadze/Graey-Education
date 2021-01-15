1cf512156218dc7da8a731e3ce7a528344ea2d27
Misho <nnamper@gmail.com>
2021-01-15 23:38:57 +0400

    ClassBasedView Example For Giorgi

--- back-end/djangotutorial/blog/urls.py
+++ back-end/djangotutorial/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import hello
+from blog.views import PostView, hello
 
 urlpatterns = [
-    path('hello/', hello)
+    path('hello/', hello),
+    path('hello/', PostView.as_view())
 ]
--- back-end/djangotutorial/blog/views.py
+++ back-end/djangotutorial/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from blog.models import Post
 
@@ -8,3 +9,13 @@ def hello(request):
         'posts': Post.objects.all()
     })
 
+
+class PostView(TemplateView):
+    queryset = Post.objects.all()
+    template_name = 'blog/hello.html'
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            'posts': self.queryset
+        }
